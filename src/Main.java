b90c88550cca1850b734cd7d36a83ca479f56c0d
Pedro <pedrogabrielcruzlima@gmail.com>
2023-03-14 23:34:37 -0300

    Primeiro commit
    Iniciar respositório

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setVisible(true);
+        janela.setSize(300,300);
+
+    }
+}
